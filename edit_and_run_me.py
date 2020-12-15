c6c082afa4139da513a85e7047feda64cbe86f09
Teskann <teskann.dev@gmail.com>
2020-12-15 11:29:31 +0100

    Reformatted file

--- edit_and_run_me.py
+++ edit_and_run_me.py
@@ -7,19 +7,18 @@ Edit and run this file to run Docapy on your project !
 """
 
 if __name__ == "__main__":
-    
     # Import Docapy core
     from docapy import html_for_project
-    
+
     # Project Name (edit it)
     project_name = "Your Project Name Here"
-    
+
     # Project Path
     project_path = "Your project path here (absolute path recommended)"
-    
+
     # Repository Link
     repo_link = "Link of the repository of this project goes here"
-    
+
     # Accent color.
     # This variable can be :
     # - "blue"
@@ -30,9 +29,9 @@ if __name__ == "__main__":
     # - "purple"
     # - "#XXXXXX" where XXXXXX is a hexadecimal color value (custom)
     color = "Accent color of the generated website goes here"
-    
+
     # Running Docapy (do not edit this part)
     html_for_project(project_path,
-                 project_name,
-                 repo_link,
-                 color)
+                     project_name,
+                     repo_link,
+                     color)
